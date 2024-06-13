0d68387fb7d6b1bffe1c9372412a2a1d15ead14c
Anushree1a <anushreenr008@gmail.com>
2024-06-13 21:07:14 +0530

    New java files added

---
+++ Arithematic.java
@@ -0,0 +1,19 @@
+public class Arithematic{
+    public static void main(String[]args){
+        int x=10;
+        int y=20;
+        int z=x+y;
+        int a=y-z;
+        int b=x*y;
+        int c=y/x;
+        int d=x%y;
+        int e=++y;
+        int f=--x;
+
+
+
+        System.out.println("Result"+z+","+a+","+b+","+c+","+d+","+e+","+f);
+
+
+    }
+}
---
+++ wideningcasting.java
@@ -0,0 +1,10 @@
+public class wideningcasting{
+    public static void main(String[]args){
+        int intNum= 9;
+        double result= intNum;
+        System.out.println("Changing Integer to Double"+result);
+
+    }
+
+
+}
