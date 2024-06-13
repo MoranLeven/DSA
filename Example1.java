45863ccb3a6f4f283778103f6558932fcb86210d
Anushree1a <anushreenr008@gmail.com>
2024-06-13 22:54:19 +0530

    Java (#3)
    * problems related to sliding window and sum of two integers
    
    * New files added
    
    * New java files added

---
+++ Example1.java
@@ -0,0 +1,9 @@
+public class Example1{
+    public static void main(String[]args){
+        int maxscore= 500;
+        int userscore=253;
+        float us= userscore;
+        float result= (us/maxscore)*100.0f;
+        System.out.println("Score percentage is: "+result);
+    }
+}
---
+++ narrowingcasting.java
@@ -0,0 +1,7 @@
+public class narrowingcasting{
+    public static void main(String[]args){
+        double dNum= 9.75d;
+        int result= (int) dNum;
+        System.out.println("Changing double to integer"+result);
+    }
+}
